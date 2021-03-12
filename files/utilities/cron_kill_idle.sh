92d6f3addd71e115cede6d19e60b6c04dd68b1ca
suwan81 <72953463+suwan81@users.noreply.github.com>
2021-03-12 14:49:05 +0900

    Delete files/utilities directory

--- files/utilities/cron_kill_idle.sh
+++
@@ -1,6 +0,0 @@
-export MON_HOME="/data/utilities"
-source ${MON_HOME}/setup/cluster_info.sh
-
-date >> /data/utilities/statlog/killed_idle.${DATE} 2>&1
-psql -AXtc " select 'select pg_terminate_backend('||pid||');'  from pg_stat_activity where state = 'idle' and  now()-query_start >= '01:00:00' ;" | psql -e >> /data/utilities/statlog/killed_idle.${DATE} 2>&1
-echo >> /data/utilities/statlog/killed_idle.${DATE}
--- files/utilities/mem_info.sh
+++
@@ -1,1 +0,0 @@
-echo `hostname` `date "+%Y-%m-%d %H:%M:%S"` `grep Commit /proc/meminfo | awk '{print $1 " " $2}'`
